f51e730ae15f275cfdd4e1244d6bc23ea90a5e56
Todd Dube <todd@thedubes.com>
2019-04-27 16:54:10 -0400

    working

--- SpaceRunner/GameResources/Library/SKTUtils/SKTEffects.swift
+++ SpaceRunner/GameResources/Library/SKTUtils/SKTEffects.swift
@@ -120,7 +120,7 @@ open class SKTRotateEffect: SKTEffect {
  * Wrapper that allows you to use SKTEffect objects as regular SKActions.
  */
 public extension SKAction {
-  public class func actionWithEffect(_ effect: SKTEffect) -> SKAction {
+   class func actionWithEffect(_ effect: SKTEffect) -> SKAction {
     return SKAction.customAction(withDuration: effect.duration) { node, elapsedTime in
       var t = elapsedTime / CGFloat(effect.duration)
 
