f9ead88ae004a8d821262c9a2f90cd09e3b5e758
Todd Dube <todd@thedubes.com>
2020-06-06 11:05:55 -0400

    rem ios inject code
    removed  ios injct

--- iOSInjectionProject/build/InjectionBundle.build/Debug-iphonesimulator/InjectionBundle.build/Objects-normal/x86_64/BundleContents.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/todddube/Dropbox/Todd/DevProjects/iOS/SpaceRunner/iOSInjectionProject/BundleContents.m \
-  /tmp/injectionforxcode/BundleInjection.h \
-  /tmp/injectionforxcode/BundleInterface.h \
-  /Users/todddube/Dropbox/Todd/DevProjects/iOS/SpaceRunner/iOSInjectionProject/BundleContents.h
